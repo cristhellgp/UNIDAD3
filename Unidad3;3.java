5494110d9838eb6c84e97eec8e55308287e43ecc
cristhellgp <143815782+cristhellgp@users.noreply.github.com>
2023-09-26 15:28:49 -0600

    Add files via upload

---
+++ Unidad3;3.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package unidad3;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Sala
+ */
+public class Unidad3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner lec=new Scanner (System.in);
+        System.out.println("ingresa el num entero");
+        int num1 = lec.nextInt();
+        if (num1%2==0);
+        System.out.println("es par");
+    }else{
+          
+        System.out.println("es impar");
+        
+          
+        // TODO code application logic here
+      
+        
+    }
+    
+}
